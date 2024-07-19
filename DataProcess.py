4ed5f6386cef451d35a34ad97913980a369bbfb3
jiduowang <1963760354@qq.com>
2024-07-19 14:52:18 +0800

    初次发布

---
+++ DataProcess.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import bpy
+
+'''
+@description: travel the folder and get the file path
+@param {string} root_directory the root directory
+@param {string} data_directory the data directory
+@param {string} output_directory the output directory
+'''
+def travel_folder(root_directory, data_directory, output_directory):
+    for root, dirs, files in os.walk(data_directory):
+        # for file in files:
+        #     file_path = os.path.join(root, file)
+        #     print(file_path)
+        process(root_directory, root, output_directory)
+
+
+'''
+@description: process the file
+@param {string} root_directory the root directory
+@param {string} dir_path the process data directory path
+@param {string} output_directory the output directory
+'''
+def process(root_directory,dir_path, output_directory):
+    tmp = dir_path.split('/')
+    current_dir_name = tmp[-1]
+    output_glb_directory = output_directory + "/" + current_dir_name + "_glb"
+    if not os.path.isdir(output_glb_directory):
+        os.mkdir(output_glb_directory)
+    #ply2glb
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            tmp, file_extension = os.path.splitext(file)
+            tmp = tmp.split('/')
+            file_name = tmp[-1]
+            if file_extension == ".ply":
+                bpy.ops.import_mesh.ply(filepath=file)
+                export_file = output_glb_directory + "/" + file_name + ".glb"
+                bpy.ops.export_scene.gltf(filepath=export_file, export_format='GLB')
+
+    #glb2b3dm
+    output_b3dm_directory = output_directory + "/" + current_dir_name + "_b3dm"
+    if not os.path.isdir(output_b3dm_directory):
+        os.mkdir(output_b3dm_directory)
+    for root, dirs, files in os.walk(output_glb_directory):
+        for file in files:
+            tmp, file_extension = os.path.splitext(file)
+            tmp = tmp.split('/')
+            file_name = tmp[-1]
+            if file_extension == ".glb":
+                input_file = file
+                output_file = output_b3dm_directory + "/" + file_name + ".b3dm"
+                os.system("node " + root_directory + '/3d-tiles-tools/tools/bin/3d-tiles-tools.js glbToB3dm '+ input_file + ' ' + output_file)
+
+
+if __name__=='__main__':
+    bpy.ops.wm.read_factory_settings(use_empty=True)
+    # Get the root directory
+    root_directory = os.getcwd()
+    # Get the data dir path
+    force_continue = True
+    for current_argument in sys.argv:
+        if force_continue:
+            if current_argument == '--':
+                force_continue = False
+            continue
+
+    #check output directory is exist
+    if not os.path.isdir(root_directory+"/output"):
+        os.mkdir(root_directory+"/output")
+
+    #try:
+        # for file in os.listdir(current_argument):
+        #     travel_folder(root_directory, current_argument, root_directory+"/output")
+    # except Exception as e:
+    #     print(e)
+    #     print("The file path is not a directory.")
+    for file in os.listdir(current_argument):
+        travel_folder(root_directory, current_argument, root_directory+"/output")
+
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # ply2b3dm
+
+## 设计初衷
+
+由于本人实习项目的原因，需要将ply文件转换为b3dm文件，但网上没有现成的方案，所以自己写了一个。
+
+## 设计思路
+
+由于ply文件和b3dm文件属于不同的联盟规范文件，所以需要将ply文件转换为gltf文件，再将gltf文件转换为b3dm文件。
+
+## 使用环境要求
+
+python3.7（不可高也不可低）
+
+包要求：numpy, typer, bpy
+
+注意bpy的安装方法为
+```
+pip install bpy==2.91a0 && bpy_post_install
+```
+
+同时克隆 [gltf-to-3d-tiles](https://github.com/xuzhusheng/gltf-to-3d-tiles) 项目到本地，根据该项目要求配置环境，并将DataProcess.py文件放到与gltf-to-3d-tiles同级目录下。
+
+与此同时，DataProcess.py文件的同级目录中创建名为data的文件夹，用于存放ply文件，文件夹格式应当如下：
+```angular2html
+--data
+  |--八甲收费站
+  |	  |---block1.ply
+  |	  |---block2.ply
+  |
+  |--一号坡
+       |--block1.ply
+       |--block2.ply
+```
+
+## 使用方法
+在DataProcess.py文件的目录中打开命令行终端（注意环境要求为python3.7），输入
+```
+python DataProcess.py ./data
+```
